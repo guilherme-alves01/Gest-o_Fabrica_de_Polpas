3fe4916b0db8a656b3913898e91ac987f7d74f65
Guilherme <guilherme.alves0@outlook.com>
2026-04-18 22:04:49 -0300

    feat: hardcode users to avoid SQLite data loss issues on Render

--- api/src/auth/auth.service.ts
+++ api/src/auth/auth.service.ts
@@ -6,11 +6,11 @@ export class AuthService {
   constructor(private prisma: PrismaService) {}
 
   async validateUser(username: string, pass: string): Promise<any> {
-    // Para teste inicial: usuário 'admin' senha 'admin'
-    if (username === 'admin' && pass === 'admin') {
-      return { username: 'admin' };
-    }
-    
+    // Usuários fixos no sistema para evitar problemas caso o banco de dados gratuito zere
+    if (username === 'admin' && pass === 'admin') return { username: 'admin' };
+    if (username === 'guilherme' && pass === 'br4s1l.X') return { username: 'guilherme' };
+    if (username === 'thais' && pass === 'l1c0r.X') return { username: 'thais' };
+
     // Se não for o teste, busca no banco
     const user = await this.prisma.user.findUnique({
       where: { username },
